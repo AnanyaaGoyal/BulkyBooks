10f648ba2d19a6b66a7519cd5ce11ed53d8be2f2
AnanyaaGoyal <ananyagoyal128@gmail.com>
2023-02-14 17:48:59 +0530

    Category List displayed

---
+++ BulkyBooks/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BulkyBooks.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBooks.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        
+    }
+
+}
