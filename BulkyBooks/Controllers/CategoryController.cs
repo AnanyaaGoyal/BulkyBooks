8d48410a49f7a7076f5cfcba30385d119c146e4f
AnanyaaGoyal <ananyagoyal128@gmail.com>
2023-02-15 17:38:48 +0530

    till video 9: edit categories

--- BulkyBooks/Controllers/CategoryController.cs
+++ BulkyBooks/Controllers/CategoryController.cs
@@ -16,5 +16,62 @@ namespace BulkyBooks.Controllers
             IEnumerable<Category> objCategoryList= _db.Categories;   
             return View(objCategoryList);
         }
+        //get
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Name and Display name are same");
+            }
+            if (ModelState.IsValid) { 
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //get
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();  
+            }
+           // var categoryFromDb = _db.Categories.FirstOrDefault(c => c.Id == id);
+            // var categoryFromDb = _db.Categories.SingleOrDefault(c => c.Id == id);
+            var categoryFromDb = _db.Categories.Find(id);
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Name and Display name are same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
